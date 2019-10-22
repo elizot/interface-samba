8eabbd1acce2e88683b0f5ad3dfa1bb48d9aea0f
brai.niac <46333317+elizot@users.noreply.github.com>
2019-10-21 22:51:46 -0300

    Add files via upload

---
+++ src/pages/navbar.php
@@ -0,0 +1,22 @@
+<div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper blue-grey darken-4 z-depth-4">
+            <a href="#" class="brand-logo logo"><img src="../assets/img/logo.svg" alt="logo"></a>
+            <a href="#" data-target="mobile-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Unidades Organizacionais</a></li>
+                <li><a href="#">Grupos e Usuários</a></li>
+                <li><a href="#"><i class="large material-icons">exit_to_app</i></a></li>
+            </ul>
+        </div>
+    </nav>
+    </div>
+
+    <ul class="sidenav" id="mobile-menu">
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Unidades Organizacionais</a></li>
+        <li><a href="#">Grupos e Usuários</a></li>
+        <li><a href="#">Sair</a></li>
+    </ul>
+
